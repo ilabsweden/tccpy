7a598e45ad9528eb95524eefb550858cb8eb59f4
Erik Billing <erik@ekhagen.org>
2025-02-21 20:19:10 +0100

    Tcc model added

--- tccpy/__init__.py
+++ tccpy/__init__.py
@@ -1,15 +1,16 @@
-# tccpy - Target Confusability Competition (TCC) model 
-This is a Python implementation of the Target Confusability Competition (TCC) model proposed by [Shurgin et al. (2020)](https://www.nature.com/articles/s41562-020-00938-0). An interactive web-version of the model is available in the [Target confusability competition model (TCC) primer](https://bradylab.ucsd.edu/tcc/). 
+"""Target Confusability Competition (TCC) model 
+This is a Python implementation of the Target Confusability Competition (TCC) model proposed by 
+Shurgin et al. (2020), (https://www.nature.com/articles/s41562-020-00938-0. An interactive web-version of the model 
+is available in the Target confusability competition model (TCC) primer, https://bradylab.ucsd.edu/tcc/.
 
-## Usage
+Usage: 
+
 The following code will run 100 iterations of TCC over four targets with familiarity 0, 0, 1, and 2:
 
     import tccpy
 
     dist = tccpy.tcc([0,0,1,2], 1, 1000)
     print(dist)
-
-## Acknowledgments
 
 Copyright [2025] [Erik Billing, https://his.se/erikb]
 
@@ -24,6 +25,6 @@ Copyright [2025] [Erik Billing, https://his.se/erikb]
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+"""
 
-
-
+from .main import tcc
---
+++ test.py
@@ -0,0 +1,3 @@
+import tccpy
+
+print(tccpy.tcc([0,0,1,2],1,1000))
