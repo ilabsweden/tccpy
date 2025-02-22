1433595c2fcc72a0de22f0a1039bc9bdae0bdc11
Erik Billing <erik@ekhagen.org>
2025-02-22 19:45:55 +0100

    Added demo notebook

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tccpy',
-    version=0.16,
+    version=0.17,
     description='A Python implementation of the Target Confusability Competition (TCC) memory model',
     packages=find_packages(),
     install_requirements=[
